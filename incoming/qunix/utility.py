35e1e1f07221b4dd75eeba6b97c72a171853037c
shuklasid19 <shuklasid19@gmail.com>
2022-09-23 16:32:31 +0530

    done

---
+++ incoming/qunix/utility.py
@@ -0,0 +1,9 @@
+def addition(a,b):
+    return a+b
+
+def subtraction(a,b):
+    return a-b
+
+print(addition(12,34))
+
+print(subtraction(34,10))
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup 
+setup( 
+    name='demoq', 
+    version='1.0', 
+    packages=['incoming'], 
+)
